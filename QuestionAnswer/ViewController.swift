f07b8bce6d2aa19058e624945ddc1339f1b36569
satoshi.a.goto <satoshi.a.goto@rakuten.com>
2019-02-13 14:30:21 +0800

    Initial Commit

---
+++ QuestionAnswer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuestionAnswer
+//
+//  Created by Goto, Satoshi a | RASIA on 13/2/19.
+//  Copyright © 2019 Goto, Satoshi a | RASIA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
